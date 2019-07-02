d9339397664a7fdd7e68edd387f47c4d2d6eae59
Manan <manansoni0@gmail.com>
2019-07-02 19:13:22 +0530

    Update car.py, add code for get_surroundings, clip max velocity

--- game/car.py
+++ game/car.py
@@ -2,56 +2,22 @@
 import numpy as np
 from PIL import Image
 import sys
-from skimage import filters, morphology, transform
+import csv
+import matplotlib.pyplot as plt
+from pprint import pprint
 
-
-def get_track(path="./test_1.jpg"):
-    def running_mean(x, N):
-        cumsum = np.cumsum(np.insert(x, 0, 0))
-        return (cumsum[N:] - cumsum[:-N]) / float(N)
-
-    # lambda function to scale an array to [min,max]
-    scale = lambda arr, min, max: min + (max - min) * (arr - np.min(arr)) / (
-        np.max(arr) - np.min(arr)
-    )
-    # lambda function get a PIL image from an array
-    get = lambda arr: Image.fromarray(scale(arr, 0, 255))
-    # open image and convert to Black and white (0-255)
-    im = Image.open(path).convert("L")
-    # display image
-    # im.show()
-    # invert image colors for better results and convert to an array for easier manipulation
-    im = 255 - np.array(im)
-    # apply median filter to remove salt and pepper noise
-    im = filters.median(im)
-    # use gaussian filter to smooth out the image
-    im = filters.gaussian(im, sigma=2.2)
-    im = scale(im, 0, 1)
-    # Reduce thick lines to single pixel lines for easier analysis
-    # Returns a thresholded image i.e All values are either 255 or 0
-    final_im = np.array(morphology.skeletonize_3d(im)).astype(int)
-    # Get the x and y co-ordinates (numpy and PIL have transpose conventions)
-    y, x = np.where(final_im == 255)
-    # Divide the y values into upper and lower parts of the track
-    x_final = []
-    y_up = []
-    y_down = []
-    for pt in np.sort(x):
-        try:
-            y1, y2 = y[np.where(x == pt)]
-            x_final.append(pt)
-            y_up.append(max(y1, y2))
-            y_down.append(min(y1, y2))
-        except:
-            pass
-    # Use a moving average to smooth out the array to remove any minor discontinuities
-    # Window size is 1% of track length to minimize the amount of data lost
-    win = int(0.01 * len(x_final))
-    y_up = running_mean(y_up, win)
-    y_down = running_mean(y_down, win)
-    x_final = x_final[: -win + 1]
-    return x_final, y_up, y_down
-
+# utility to read csv track files into numpy arrays
+def read_track(in_file='sample_path.csv',scale=1000):
+    x = []
+    y1 = []
+    y2 = []
+    with open(in_file,'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            x.append(float(row[0]))
+            y1.append(float(row[1]))
+            y2.append(float(row[2]))
+    return scale*np.array(x), scale*np.array(y1), scale*np.array(y2)
 
 def normalize(v):
     norm = np.linalg.norm(v)
@@ -59,11 +25,9 @@ def normalize(v):
         return v
     return v / norm
 
-
 class car:
-    eyes = np.array([[0, 1], [0, -1]])
-
     def __init__(self, track):
+        #self.eyes = np.array([[0, 1], [0, -1]])
         self.track = track
         self.track_at = lambda x: (
             np.interp(x, self.track[0], self.track[1]),
@@ -72,6 +36,7 @@ class car:
         self.pos = np.array((track[0][0], (track[1][0] + track[2][0]) / 2))
         self.accl = np.zeros((2,))
         self.vel = np.zeros((2,))
+        self.max_vel = np.array([25,25])
         self.last_pos = np.zeros((2,))
         self.pos_history = [np.copy(self.pos)]
         self.vel_history = [np.copy(self.vel)]
@@ -80,10 +45,23 @@ class car:
 
     def is_legal(self):
         t = self.track_at(self.pos[0])
-        return (t[0] > self.pos[1] > t[1]) and (self.pos[0] < self.end)
+        return (t[0] < self.pos[1] < t[1]) and (self.pos[0] < self.end)
+
+    def get_rot_mat(self,th):
+        return np.array([[np.cos(th),-np.sin(th)],[np.sin(th),np.cos(th)]])
 
     def update(self):
         self.last_pos = np.copy(self.pos)
+
+        if self.vel[0] > self.max_vel[0]:
+            self.vel[0] = self.max_vel[0]
+        if self.vel[0] < 0:
+            self.vel[0] = 0
+        if self.vel[1] > self.max_vel[1]:
+            self.vel[1] = self.max_vel[1]
+        if self.vel[1] < -self.max_vel[1]:
+            self.vel[1] = -self.max_vel[1]
+
         self.pos += self.vel
         if self.is_legal():
             self.vel += self.accl
@@ -91,29 +69,46 @@ class car:
             self.pos = self.last_pos
             self.vel = np.zeros((2,))
             self.accl = np.zeros((2,))
+
         self.pos_history.append(np.copy(self.pos))
         self.vel_history.append(np.copy(self.vel))
         self.accl_history.append(np.copy(self.accl))
+        #plt.scatter(self.get_surrounding(),50,color='y')
 
-    def get_surrounding():
-        # rotate eyes
-        if np.linalg.norm(self.vel) != 0:
-            v1 = normalize(self.vel)
-            rot_matrix = np.array([v1, [-v1[0], v1[1]]]).T
+    def get_surrounding(self):
+
+        rot_mat = self.get_rot_mat(90)
+
+        if np.linalg.norm(self.vel) == 0.0:
+            print('--')
+            dist = self.track_at(self.pos)
         else:
-            rot_matrix = np.eye(2)
-            # 2 is the num of dimensions
-        rotated_eyes = rot_matrix.dot(eyes)
-        # TODO get return distances in the direction of eyes
-        # TODO add max speed limit
+            # upper track
+            p1 = self.pos
+            p2 = self.pos + (np.dot(rot_mat,self.vel.T)).T
+            #print(p1,p2)
+            p3 = np.vstack([track[0], track[2]]).T
 
+            dist = np.abs(np.cross(p2-p1,p3-p1)/np.linalg.norm(p2-p1))
+        return track[0][np.argmin(dist)]
 
-"""
-my_car1 =car(get_track())
-my_car1.accl = np.array((0, -0.3))
-my_car1.vel = np.array((3.0,0))
+track = read_track('test_2.csv')
+my_car1 = car(track)
+my_car1.accl = np.array((0.0, 0.0))
+my_car1.vel = np.array((15.0,0.0))
+
 for i in range(100):
     my_car1.update()
-plt.scatter(*zip(*my_car1.pos_history))
-plt.show()
+
+my_car1.get_surrounding()
+
 """
+for i in range(np.array(my_car1.pos_history)[:,0].shape[0]):
+        plt.arrow(np.array(my_car1.pos_history)[i,0],np.array(my_car1.pos_history)[i,1],
+            np.array(my_car1.vel_history)[i,0],np.array(my_car1.vel_history)[i,1],color='r')
+"""
+
+plt.scatter(track[0],track[1],c='k')
+plt.scatter(track[0],track[2],c='k')
+plt.plot(*zip(*my_car1.pos_history),c='b')
+plt.show()
