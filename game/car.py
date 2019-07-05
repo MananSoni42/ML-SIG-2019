c976fd806446341ebae548fbe8f3985bf9d212d6
Siddharth Singh <sidharth_0903@hotmail.com>
2019-07-05 14:53:42 +0530

    Minor changes for readability

--- game/car.py
+++ game/car.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from pprint import pprint
 
 # utility to read csv track files into numpy arrays
-def read_track(in_file='tracks/sample_path.csv', scale=[1000,600]):
+def read_track(in_file="tracks/sample_path.csv", scale=[1000, 600]):
     x = []
     y1 = []
     y2 = []
@@ -17,7 +17,8 @@ def read_track(in_file='tracks/sample_path.csv', scale=[1000,600]):
             x.append(float(row[0]))
             y1.append(float(row[1]))
             y2.append(float(row[2]))
-    return scale[0] * np.array(x), scale[1] * np.array(y1), scale[1]*np.array(y2)
+    return scale[0] * np.array(x), scale[1] * np.array(y1), scale[1] * np.array(y2)
+
 
 def normalize(v):
     norm = np.linalg.norm(v)
@@ -27,7 +28,8 @@ def normalize(v):
 
 
 class car:
-    def __init__(self, track):
+    def __init__(self, track, accl_function):
+        self.accl_function = accl_function
         self.eyes = np.array([[0, 1], [0, -1], [1, 1], [1, -1]])
         self.track = track
         self.track_at = lambda x: (
@@ -35,7 +37,7 @@ class car:
             np.interp(x, self.track[0], self.track[2]),
         )
         self.pos = np.array((track[0][0], (track[1][0] + track[2][0]) / 2))
-        #self.pos = np.array((track[0][0], track[1][0]+10))
+        # self.pos = np.array((track[0][0], track[1][0]+10))
         self.max_dist = 1000
         self.accl = np.zeros((2,))
         self.vel = np.zeros((2,))
@@ -54,27 +56,29 @@ class car:
     def get_rot_mat(self, th):
         return np.array([[np.cos(th), -np.sin(th)], [np.sin(th), np.cos(th)]])
 
-    def accl_function(self, params):
-        dist = params[:4]
-        du, dd, _, _ = dist
-        last_dist = params[4:8]
-        last_vel = params[8:10]
+    def default_accl_function(
+        distances, last_distances, current_velocity, previous_velocity
+    ):
+        du, dd, _, _ = distances
+        accl = [0, 0]
+        accl[0] = 0.1  # Accel along x axis
 
-        self.accl[0] = 0.1
-        #self.accl[1] = (dd-du)/(du+dd)
-        self.accl[1] = (1+du)/(1+dd)-1
+        # Accel along y axis which takes into account the distances from the tracks
+        accl[1] = (1 + du) / (1 + dd) - 1
 
-        if dist[0] < 200 or dist[2] < 200:
-            self.accl[1] -= 0.2
+        if distances[0] < 200 or distances[2] < 200:
+            accl[1] -= 0.2
 
-        if dist[1] < 200 or dist[3] < 200:
-            self.accl[1] += 0.2
+        if distances[1] < 200 or distances[3] < 200:
+            accl[1] += 0.2
+
+        return np.array(accl)
 
     def run(self):
-        dist = self.get_surrounding(self.pos,self.vel)
-        last_dist = self.get_surrounding(self.last_pos,self.last_vel)
-        params = [*dist,*last_dist,*self.last_vel]
-        self.accl_function(params)
+        dist = self.get_surrounding(self.pos, self.vel)
+        last_dist = self.get_surrounding(self.last_pos, self.last_vel)
+        # params = [*dist,*last_dist,*self.last_vel]
+        self.accl = self.accl_function(dist, last_dist, self.vel, self.last_vel)
         self.update()
 
     def update(self):
@@ -106,8 +110,8 @@ class car:
         plt.plot(self.track[0], self.track[2], c="k")
         plt.plot(*zip(*self.pos_history))
 
-        plt.gca().set_xlim(-10,1010)
-        plt.gca().set_ylim(-10,610)
+        plt.gca().set_xlim(-10, 1010)
+        plt.gca().set_ylim(-10, 610)
         plt.show()
 
     def utility(self):
@@ -117,12 +121,12 @@ class car:
             max_dist: maximum distance travelled by the car
             time: time taken to finish the track (-1 if car did not reach the end)
         """
-        x_pos = np.array(self.pos_history)[:,0]
+        x_pos = np.array(self.pos_history)[:, 0]
         max_dist = np.max(x_pos)
-        time =  np.argmax(x_pos) if max_dist >= self.end-2 else -1
-        return max_dist,time
+        time = np.argmax(x_pos) if max_dist >= self.end - 2 else -1
+        return max_dist, time
 
-    def get_surrounding(self,pos,vel):
+    def get_surrounding(self, pos, vel):
         if np.linalg.norm(vel) != 0:
             v1 = normalize(vel)
             rot_matrix = np.array([v1, [-v1[1], v1[0]]]).T
@@ -140,20 +144,28 @@ class car:
             # take upper track if line of vision is above 0
             if self.eyes[i][1] > 0:
                 # find point on vision_tensor closest to track
-                idx = np.argwhere(np.diff(np.sign(vision_tensor[:,i,1] - self.track[2]))).flatten()
-                #plt.scatter(*vision_tensor[idx,i].reshape(2,))
+                idx = np.argwhere(
+                    np.diff(np.sign(vision_tensor[:, i, 1] - self.track[2]))
+                ).flatten()
+                # plt.scatter(*vision_tensor[idx,i].reshape(2,))
 
             # else take lower track
             else:
                 # find point on vision_tensor closest to track
-                idx = np.argwhere(np.diff(np.sign(vision_tensor[:,i,1] - self.track[1]))).flatten()
-                #plt.scatter(*vision_tensor[idx,i].reshape(2,))
+                idx = np.argwhere(
+                    np.diff(np.sign(vision_tensor[:, i, 1] - self.track[1]))
+                ).flatten()
+                # plt.scatter(*vision_tensor[idx,i].reshape(2,))
 
             try:
-                dists[i] = np.sqrt(np.sum(np.square(vision_tensor[idx,i].reshape(2,)-pos.reshape(2,))))
+                dists[i] = np.sqrt(
+                    np.sum(np.square(vision_tensor[idx, i].reshape(2) - pos.reshape(2)))
+                )
             except ValueError:
                 dists[i] = self.max_dist
         return dists
+
+
 """
 track = read_track()
 my_car1 = car(track)
--- game/example.py
+++ game/example.py
@@ -1,15 +1,37 @@
-from car import car,read_track
+from car import car, read_track
+import numpy as np
 from animate_pyglet import animate_cars
 
-track_path = 'tracks/test_3.csv'
+track_path = "tracks/test_3.csv"
 
 track = read_track(track_path)
-my_car = car(track)
+
+
+def example_accl_function(
+    distances, last_distances, current_velocity, previous_velocity
+):
+    du, dd, _, _ = distances
+    accl = [0, 0]
+    accl[0] = 0.1  # Accel along x axis
+
+    # Accel along y axis which takes into account the distances from the tracks
+    accl[1] = (1 + du) / (1 + dd) - 1
+
+    if distances[0] < 200 or distances[2] < 200:
+        accl[1] -= 0.2
+
+    if distances[1] < 200 or distances[3] < 200:
+        accl[1] += 0.2
+
+    return np.array(accl)
+
+
+my_car = car(track, example_accl_function)
 
 for i in range(2000):
     my_car.run()
 
 my_car.plot_history()
-#print(my_car.utility())
+# print(my_car.utility())
 
-animate_cars([my_car],track_path=track_path)
+animate_cars([my_car], track_path=track_path)
